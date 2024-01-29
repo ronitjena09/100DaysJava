b1dc31b942d01c03841d54f21da882df5f70ba10
Ronit Jena <ronitjena64@gmail.com>
2024-01-29 23:20:57 +0530

    regular commit

--- day1/patternCheck.java
+++ day1/patternCheck.java
@@ -1,6 +1,8 @@
-// to print a pattern of stars 
+package day1;
+
 public class patternCheck {
     public static void main(String[] args) {
+        // to print a pattern of stars 
       System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
     }
 }
---
+++ day1/variables_and_datatypes.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class variables_and_datatypes {
+        public static void main(String[] args) {
+          // To print Variables and datatypes
+          String name = "Ronit Jena"; //String Datatype
+          int num = 10; //integer Datatype
+          double digit = 10.24; // double datatype
+          char a = 'a'; // Char datatype
+          boolean check = true; // boolean datatype
+      
+          System.out.println(name);
+          System.out.println(num);
+          System.out.println(digit);
+          System.out.println(a);
+          System.out.println(check);
+        }
+}
