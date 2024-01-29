65c61c296cacc19290d522043a7dc3b0b251a02e
Ronit Jena <ronitjena64@gmail.com>
2024-01-29 23:47:56 +0530

    regular commit

---
+++ day1/sumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package day1;
+
+import java.util.Scanner;
+
+public class sumOfTwoNumbers {
+    public static void main(String[] args) {
+    // Sum of two user definer numbers
+   Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    
+    System.out.println("The sum is: "+(a+b));
+  }
+}
