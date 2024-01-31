829cd9b01870d34ad7cd2e3ee12b2a17a2c5e6bc
Ronit Jena <ronitjena64@gmail.com>
2024-01-31 15:15:11 +0530

    reqular commit

---
+++ day2/compareNums.java
@@ -0,0 +1,22 @@
+package day2;
+import java.util.*;
+public class compareNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // To compare 2 nums
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        if ( num1 == num2){
+          System.out.println("The numbers are equal");
+        }
+        else {
+          if( num1 > num2) {
+            System.out.println(num1 + "is larger than " +num2);
+          }
+          else {
+            System.out.println(num1 + "is smaller than " +num2);
+            
+          }
+        }
+      }
+}
