eeedbdc264960cbd924ee059a668714fee0d9544
Ronit Jena <ronitjena64@gmail.com>
2024-01-31 14:42:26 +0530

    regular commit

---
+++ day2/checkTeenager.java
@@ -0,0 +1,16 @@
+package day2;
+import java.util.*;
+public class checkTeenager {
+    {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if( age <= 20){
+          System.out.println("You are a teenager");
+          
+        }
+        else {
+          System.out.println("You are not a teenager");
+          
+        }
+}
+}
