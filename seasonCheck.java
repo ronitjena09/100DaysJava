b7b3113d879bf7227377681055d6af579a246c54
Ronit Jena <ronitjena64@gmail.com>
2024-02-01 19:20:56 +0530

    regular commit

---
+++ seasonCheck.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class seasonCheck {
+    public static void main(String[] args) {
+    //
+    Scanner sc = new Scanner(System.in);
+    String seasonCheck = sc.nextLine();
+    switch(seasonCheck){
+      case "January":
+      case "February":
+        System.out.println("Winter Season");
+        break;
+      case "March":
+      case "April":
+        System.out.println("Spring");
+        break;
+      case "May":
+      case "June":
+      case "July":
+        System.out.println("Summer");
+        break;
+      case "August":
+      case "September":
+        System.out.println("Rainy Season");
+        break;
+      case "October":
+      case "November":
+        System.out.println("Autumn Season");
+        break;
+      
+      case "December":
+        System.out.println("Winter Season");
+        break;
+      default:
+        System.out.println("Invalid");
+    }
+  }
+}
