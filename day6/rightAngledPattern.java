59c55f24a8bb96da2bf93ac75fa0ef7207b6660b
Ronit Jena <ronitjena64@gmail.com>
2024-02-11 11:57:49 +0530

    regular commit

---
+++ day6/rightAngledPattern.java
@@ -0,0 +1,20 @@
+package day6;
+
+import java.util.Scanner;
+
+public class rightAngledPattern {
+    
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int rows = sc.nextInt();
+    for(int i=0;i<rows;i++){
+      for(int j = 0; j<=i;j++){
+        System.out.print("* ");
+        
+      }
+      System.out.println();
+    }
+  }
+}
