d3bbd3480175b9ea9ff11a1988f9e6fb6a871612
Ronit Jena <ronitjena64@gmail.com>
2024-02-15 23:06:54 +0530

    regular commit

---
+++ day10/stringConcatenation.java
@@ -0,0 +1,13 @@
+package day10;
+
+import java.util.Scanner;
+
+public class stringConcatenation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.nextLine();
+        String lastName = sc.nextLine();
+        System.out.println(firstName + " " + lastName);
+
+    }
+}
